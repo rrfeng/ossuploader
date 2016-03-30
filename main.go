30c13f667766615ed323ade5c632af04914313c9
rrfeng <rengaofeng@gotokeep.com>
2016-03-30 14:43:01 +0800

    read secret from config file for security purpose

--- main.go
+++ main.go
@@ -1,51 +1,74 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"io/ioutil"
 	"log"
+	"os"
 	"path/filepath"
 )
 
-var src *string = flag.String("i", "", "File to upload")
-var key *string = flag.String("o", "", "Name to save file in bucket")
-var bkt *string = flag.String("b", "", "Bucket to store file")
-var accesskey *string = flag.String("a", "", "AccessKey of OSS account")
-var secretkey *string = flag.String("s", "", "SecretKey of OSS account")
-var routines *int = flag.Int("r", 1, "Number of routines for parallel upload")
-var endpoint *string = flag.String("e", "", "Endpoint of ali OSS service")
+var src *string = flag.String("i", "", "File to upload.")
+var key *string = flag.String("o", "", "Name to save file in bucket. Default is the name of uploading file.")
+var bkt *string = flag.String("b", "", "Bucket to store file.")
 var delete *bool = flag.Bool("d", false, "Delete action.")
+var cfgfile *string = flag.String("c", "", "Location of config file. Default is \"$HOME/.ossuploader/config.json\".")
+var routines *int = flag.Int("r", 1, "Number of routines for parallel upload.")
+var endpoint *string = flag.String("e", "", "Endpoint of ali OSS service.")
+
+type config struct {
+	AccessKey string `json: accesskey`
+	SecretKey string `json: secretkey`
+}
 
 func main() {
 	flag.Parse()
 
 	switch {
-	case *src == "" && *delete == false:
-		log.Panic("No input file!")
+	case *delete == false && *src == "":
+		log.Fatalln("No input file!")
+	case *delete == false && *key == "":
+		*key = *src
+	case *delete && *key == "":
+		log.Fatalln("Delete action need objectKey!")
 	case *bkt == "":
-		log.Panic("No bucket name!")
-	case *accesskey == "":
-		log.Panic("No access key!")
-	case *secretkey == "":
-		log.Panic("No secret key!")
+		log.Fatalln("No bucket name!")
 	case *endpoint == "":
-		log.Panic("Please set the oss endpoint!")
+		log.Fatalln("Please set the oss endpoint!")
+	case *cfgfile == "":
+		*cfgfile = os.Getenv("HOME") + "/.ossuploader/config.json"
 	}
 
-	client, err := oss.New(*endpoint, *accesskey, *secretkey)
+	var cfg config
+	if _, err := os.Stat(*cfgfile); os.IsNotExist(err) {
+		log.Fatalln(err.Error())
+	} else {
+		data, err := ioutil.ReadFile(*cfgfile)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+		err = json.Unmarshal(data, &cfg)
+		if err != nil {
+			log.Fatalln(err.Error())
+		}
+	}
+
+	client, err := oss.New(*endpoint, cfg.AccessKey, cfg.SecretKey)
 	if err != nil {
-		log.Panic(err.Error())
+		log.Fatalln(err.Error())
 	}
 
 	bucket, err := client.Bucket(*bkt)
 	if err != nil {
-		log.Panic(err.Error())
+		log.Fatalln(err.Error())
 	}
 
 	if *delete {
 		err = bucket.DeleteObject(*key)
 		if err != nil {
-			log.Panic(err.Error())
+			log.Fatalln(err.Error())
 		} else {
 			log.Println(*key + " deleted!")
 		}
@@ -54,7 +77,7 @@ func main() {
 
 		err = bucket.UploadFile(*key, *src, 10*1024*1024, oss.Routines(*routines), oss.Checkpoint(true, "/tmp/."+f_name+".osstmp"))
 		if err != nil {
-			log.Panic(err.Error())
+			log.Fatalln(err.Error())
 		} else {
 			log.Println(*src + " upload success!")
 		}
